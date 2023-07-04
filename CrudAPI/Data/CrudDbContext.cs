95d7583cd8c40715111b0927093479b3e97a890e
Gustavo Penagos <110733999+GustavoPenagos@users.noreply.github.com>
2023-07-04 17:14:31 -0500

    creacion dbcontext

---
+++ CrudAPI/Data/CrudDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CrudAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudAPI.Data
+{
+    public class CrudDbContext: DbContext
+    {
+        public CrudDbContext(DbContextOptions<CrudDbContext>options):base(options)
+        {
+        }
+        public DbSet<Alumnos> Alumnos { get; set; }
+        public DbSet<Asignatura> Asignatura { get; set;}
+        public DbSet<Profesor> Profesor { get; set;}
+    }
+}
---
+++ CrudAPI/Data/migrations/20230704215023_initial.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CrudAPI.data.migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Alumnos",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Edad = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Direccion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Telefono = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Asignatura = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Calificacion = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alumnos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Asignatura",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Asignatura", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Profesor",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Edad = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Direccion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Telefono = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Asignatura = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profesor", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Alumnos");
+
+            migrationBuilder.DropTable(
+                name: "Asignatura");
+
+            migrationBuilder.DropTable(
+                name: "Profesor");
+        }
+    }
+}
